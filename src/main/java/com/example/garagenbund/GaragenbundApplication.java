bea138a013ad487396df468900ba8a2bd82ebf72
Whites <derwhites@gmail.com>
2023-11-18 15:18:19 +0100

    Project initialised

---
+++ src/main/java/com/example/garagenbund/GaragenbundApplication.java
@@ -0,0 +1,13 @@
+package com.example.garagenbund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaragenbundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaragenbundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/garagenbund/GaragenbundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.garagenbund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaragenbundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
