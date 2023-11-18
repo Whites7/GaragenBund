40a446ca86c32155e2a8b0353888b9f556a59826
Whites <derwhites@gmail.com>
2023-11-18 16:14:47 +0100

    Index Page mit Beispiel Form erstellt und anzeigen lassen mit Thymeleaf

--- src/main/java/com/example/garagenbund/GaragenbundApplication.java
+++ src/main/java/com/example/garagenbund/GaragenbundApplication.java
@@ -1,7 +1,12 @@
 package com.example.garagenbund;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class GaragenbundApplication {
@@ -9,5 +14,6 @@ public class GaragenbundApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GaragenbundApplication.class, args);
 	}
+
 
 }
