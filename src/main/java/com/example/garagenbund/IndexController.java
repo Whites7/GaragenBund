15eec7e29b6eeec3584bec57a35a0dd728d0414f
Whites7 <137267028+Whites7@users.noreply.github.com>
2023-11-27 09:15:35 +0100

    Cleanup: - Removed unused Imports - Removed Commented Code

--- src/main/java/com/example/garagenbund/IndexController.java
+++ src/main/java/com/example/garagenbund/IndexController.java
@@ -1,18 +1,14 @@
 package com.example.garagenbund;
 
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
+@SuppressWarnings("unused")
 @Controller
 public class IndexController {
 
@@ -28,11 +24,6 @@ public class IndexController {
 
     @PostMapping("/uploads")
     public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
-        //StringBuilder fileNames = new StringBuilder();
-        //Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
-        //fileNames.append(file.getOriginalFilename());
-        //Files.write(fileNameAndPath, file.getBytes());
-        //model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
 
         FileUploadUtil.saveFile(UPLOAD_DIRECTORY, file.getOriginalFilename(), file);
 
